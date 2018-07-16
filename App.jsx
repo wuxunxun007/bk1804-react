90cd77e86852fd7d1186bd8fa4706421e82dd8ce
SuperLjm <852202413@qq.com>
2018-07-16 20:16:37 +0800

    高星提交的

--- App.jsx
+++ App.jsx
@@ -23,7 +23,7 @@ class App extends Component {
   render () {
     return (
       <div className = 'container'>
-        高星来了
+      是大多数
         <Switch>
           {
             this.state.routes.map((item,index) => (
